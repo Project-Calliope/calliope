f9e61ecf0be400755a3031b421cf726b5293642b
Adrien LAROUSSE <125043815+adrien-lrsse@users.noreply.github.com>
2025-03-17 23:02:25 -0400

    feat: postgres integration

--- calliope-backend/index.js
+++ calliope-backend/index.js
@@ -19,8 +19,40 @@ app.get("/", (req, res) => {
   res.send("Calliope Backend is running!");
 });
 
-app.get("/api", (req, res) => {
-  res.send(`${process.env.JWT_TOKEN}`);
+app.get("/test/env", (req, res) => {
+  res.send(`${process.env.ENV}`);
+});
+
+// DB Test connection
+const pool = require("./config/pg.config");
+app.get("/test/db", async (req, res) => {
+  try {
+    const client = await pool.connect();
+
+    // Exécuter une requête pour récupérer l'heure, l'utilisateur connecté et la base de données
+    const result = await client.query(`
+      SELECT NOW() AS current_time, 
+             current_user AS connected_user, 
+             current_database() AS connected_database;
+    `);
+
+    client.release();
+
+    res.json({
+      success: true,
+      message: "Connexion à la base de données réussie",
+      serverTime: result.rows[0].current_time,
+      connectedUser: result.rows[0].connected_user,
+      connectedDatabase: result.rows[0].connected_database,
+    });
+  } catch (error) {
+    console.error("Erreur de connexion à la base de données:", error);
+    res.status(500).json({
+      success: false,
+      message: "Erreur de connexion à la base de données",
+      error: error.message,
+    });
+  }
 });
 
 const audioRoutes = require("./routes/audio.routes");
