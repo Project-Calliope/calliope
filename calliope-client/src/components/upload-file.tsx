c9a8ea84d7452a7e01fb504d5210f14e64ddae6f
Adrien LAROUSSE <adrien.larousse@telecomnancy.eu>
2025-03-19 11:29:27 -0400

    feat(client): add shadcn alert dialog for send audio files

--- calliope-client/src/components/upload-file.tsx
+++ calliope-client/src/components/upload-file.tsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
-import { Dialog } from "@radix-ui/themes";
 import AudioService from "../services/AudioService";
 import { Button } from "@/components/ui/button";
 import toast from "react-hot-toast";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 const FileUploadDialog = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
@@ -21,7 +29,6 @@ const FileUploadDialog = () => {
       try {
         const response = await AudioService.upload(selectedFile);
         console.log("Réponse de l'API :", response);
-        setIsDialogOpen(false); // Fermer la boîte de dialogue après l'envoi
         toast.success("Fichier envoyé avec succès !");
       } catch (error) {
         if (error instanceof Error) {
@@ -36,58 +43,31 @@ const FileUploadDialog = () => {
   };
 
   return (
-    <Dialog.Root
-      open={isDialogOpen}
-      onOpenChange={() => setIsDialogOpen(!isDialogOpen)}
-    >
-      {/* Bouton pour ouvrir la modale */}
-      <Dialog.Trigger>
-        <Button>Envoyer un fichier</Button>
-      </Dialog.Trigger>
-
-      <Dialog.Content
-        className="p-4 bg-white rounded-lg shadow-lg"
-        style={{ zIndex: 1 }}
-      >
-        <Dialog.Title className="text-xl font-bold">
-          Envoyer un fichier
-        </Dialog.Title>
-        <Dialog.Description>
-          Sélectionnez un fichier à transcrire.
-        </Dialog.Description>
-
-        {/* Formulaire d'upload */}
-        <form onSubmit={handleSubmit} className="mt-4">
-          <input
-            type="file"
-            accept="audio/mpeg, audio/wav, audio/mp3, audio/m4a, audio/x-wav"
-            onChange={handleFileChange}
-            className="block w-full border p-2 rounded-md"
-          />
-          <Button
-            type="submit"
-            color="blue"
-            variant="destructive"
-            className="mt-4"
-            disabled={!selectedFile}
-          >
-            Envoyer
-          </Button>
-        </form>
-
-        {/* Bouton pour fermer le dialogue */}
-        <Dialog.Close>
-          <Button
-            color="red"
-            variant="destructive"
-            className="mt-4"
-            onClick={() => setIsDialogOpen(false)}
-          >
-            Fermer
-          </Button>
-        </Dialog.Close>
-      </Dialog.Content>
-    </Dialog.Root>
+    <>
+      <AlertDialog>
+        <AlertDialogTrigger>
+          <Button variant="outline">Envoyer un fichier</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogTitle>Envoyer un fichier</AlertDialogTitle>
+          <AlertDialogDescription>Sélection</AlertDialogDescription>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="file"
+              accept="audio/mpeg, audio/wav, audio/mp3, audio/m4a, audio/x-wav"
+              onChange={handleFileChange}
+              className="block w-full border p-2 rounded-md"
+            />
+            <AlertDialogFooter className="flex justify-end gap-2 mt-2">
+              <AlertDialogAction type="submit" disabled={!selectedFile}>
+                Envoyer
+              </AlertDialogAction>
+              <AlertDialogCancel>Annuler</AlertDialogCancel>
+            </AlertDialogFooter>
+          </form>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
 
