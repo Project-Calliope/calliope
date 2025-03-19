3886d7824f58a6f1b2d7321e2ae78ff27cd6edc0
Yann Dionisio <ydmail02@gmail.com>
2025-03-19 15:22:13 -0400

    feat(model): integrate Whisper model for audio transcription in ModelManager

--- calliope-ai/service/model_manager.py
+++ calliope-ai/service/model_manager.py
@@ -1,9 +1,24 @@
+"""ToDo : To refactor !!"""
+
+import whisper
+
+class Model:
+    
+    def __init__(self):
+        self.model = whisper.load_model("turbo")
+
+    def predict(self, input_data):
+        return self.model.transcribe(input_data)
+
+
 class ModelManager:
 
     def __init__(self):
         self.model = None
 
     def load_model(self, model):
+        if model = "turbo":
+            self.model = Model()
         self.model = model
     
     def predict(self, input_data):
