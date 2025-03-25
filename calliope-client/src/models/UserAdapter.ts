c84bbe185fffdde49343e221887375dcba41f41b
Adrien LAROUSSE <adrien.larousse@telecomnancy.eu>
2025-03-24 22:03:55 -0400

    fix(client): define any

--- calliope-client/src/models/UserAdapter.ts
+++ calliope-client/src/models/UserAdapter.ts
@@ -1,7 +1,9 @@
 import User from "@/models/User";
 
 class UserAdapter {
-  public static adapt(data: any): User {
+  public static adapt(data: {
+    user: { public_user_id: string; username: string; email: string };
+  }): User {
     return new User(
       data.user.public_user_id,
       data.user.username,
