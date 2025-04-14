b73629b92e55d867335f1dcdc11ed7dc8e770483
Victor Levrel <levv1279@USherbrooke.ca>
2025-04-14 19:11:55 -0400

    docs(backend): adding docs in miidleware and models

--- calliope-backend/middleware/jwtMiddleware.js
+++ calliope-backend/middleware/jwtMiddleware.js
@@ -3,6 +3,12 @@ const jwtConfig = require("../config/jwt.config");
 const invalidToken = [];
 
 exports.verifyToken = (req, res, next) => {
+  /**
+   * Extracts the JWT token from the Authorization header of the request.
+   * The header is expected to follow the format: "Bearer <token>".
+   *
+   * @type {string | undefined} token - The extracted JWT token, or undefined if the header is missing or improperly formatted.
+   */
   const token = req.headers.authorization?.split(" ")[1]; // Extract token from "Bearer <token>"
 
   if (!token) {
@@ -32,6 +38,12 @@ exports.verifyToken = (req, res, next) => {
 };
 
 exports.invalidateToken = (req, res) => {
+  /**
+   * Extracts the JWT token from the Authorization header in the request.
+   * The header is expected to follow the format: "Bearer <token>".
+   *
+   * @type {string | undefined} token - The extracted JWT token, or undefined if the header is missing or improperly formatted.
+   */
   const token = req.headers.authorization?.split(" ")[1]; // Extract token from "Bearer <token
   invalidToken.push(token);
   res.status(200).json({
